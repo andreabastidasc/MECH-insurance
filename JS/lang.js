7a3f8b5134ee12ebd9f193196e529b741962f8dc
Andrea Bastidas <andrea.bcolina@gmail.com>
2023-12-21 22:24:33 -0300

    add languages and translate navbar

---
+++ JS/lang.js
@@ -0,0 +1,21 @@
+addEventListener("DOMContentLoaded", (event) => {
+    const langElement = document.getElementById("lang");
+    const langTextElements = document.querySelectorAll("[data-section]");
+
+    langElement.addEventListener("click", (e) => {
+        changeLanguage(e.target.value)
+    })
+
+    const changeLanguage = async language => {
+        const requestJson = await fetch(`../lang/${language}.json`);
+        const texts = await requestJson.json();
+
+        for(const langTextElement of langTextElements) {
+            const section = langTextElement.dataset.section;
+            const value = langTextElement.dataset.value;
+
+            langTextElement.innerHTML = texts[section][value]
+        }
+    }
+});
+
