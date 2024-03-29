d73f511477c0a6578d6ee64a9a96c707b283656d
Andrea Bastidas <andrea.bcolina@gmail.com>
2024-03-28 22:32:02 -0300

    add lang to localStorage

--- JS/lang.js
+++ JS/lang.js
@@ -1,21 +1,26 @@
 addEventListener("DOMContentLoaded", (event) => {
     const langElement = document.getElementById("lang");
     const langTextElements = document.querySelectorAll("[data-section]");
+    let selectedLanguage = localStorage.getItem('selectedLanguage') || 'defaultLanguage'; // Default language if none selected
 
-    langElement.addEventListener("click", (e) => {
-        changeLanguage(e.target.value);
-    })
+    // Set initial language based on localStorage or default
+    changeLanguage(selectedLanguage);
 
-    const changeLanguage = async language => {
+    langElement.addEventListener("change", (e) => {
+        selectedLanguage = e.target.value;
+        localStorage.setItem('selectedLanguage', selectedLanguage);
+        changeLanguage(selectedLanguage);
+    });
+
+    async function changeLanguage(language) {
         const requestJson = await fetch(`https://raw.githubusercontent.com/andreabastidasc/MECH-insurance/main/lang/${language}.json`);
         const texts = await requestJson.json();
 
-        for(const langTextElement of langTextElements) {
+        for (const langTextElement of langTextElements) {
             const section = langTextElement.dataset.section;
             const value = langTextElement.dataset.value;
 
-            langTextElement.innerHTML = texts[section][value]
+            langTextElement.innerHTML = texts[section][value];
         }
     }
 });
-
